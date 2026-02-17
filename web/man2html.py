60dc0480124795634ca933b8cc0c39a64e60e57e
Gino Curtis <ginocurtis5@gmail.com>
2026-02-16 21:05:36 -0700

    Switch from <br> to <hr>

--- web/man2html.py
+++ web/man2html.py
@@ -49,7 +49,7 @@ class ManHTML:
             sys.exit(f"No matches found in h1 block")
 
         new_line = '<a href="../">Go Back to Browsing Local Man Pages</a>'
-        self.html = "\n".join(lines[:insert_index] + [new_line, "<br>"] + lines[insert_index:])
+        self.html = "\n".join(lines[:insert_index] + [new_line, "<hr>"] + lines[insert_index:])
 
     def write_html(self, directory=None):
         if directory:
