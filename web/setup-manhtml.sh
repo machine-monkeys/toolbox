72f8fabb63f436187f542a90fa463a30542531ee
Gino <gino@blackbox.home.arpa>
2026-02-16 21:55:04 -0700

    add: setup script for manhtml

---
+++ web/setup-manhtml.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BIN="$HOME/.local/bin/"
+
+# python man2html.py
+mkdir -p "$BIN"
+cp man2html.py "$BIN/man2html"
+chmod +x "$BIN/man2html"
+echo "Copied man2html to ~/.local/bin/"
