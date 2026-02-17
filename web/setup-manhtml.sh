68e982624412a78d4fd80b3da7481a6c9c1d4fb3
CurtisGino <curtis.gino@rincon.com>
2026-02-17 09:46:03 -0700

    edit: man2html directory setup

--- web/setup-manhtml.sh
+++ web/setup-manhtml.sh
@@ -2,6 +2,14 @@
 
 BIN="$HOME/.local/bin/"
 
+if [[ -d "/srv/www" ]]; then
+    mkdir -p /srv/www/man
+    mkdir -p /srv/www/css
+    mkdir -p /srv/www/man/{A..Z}
+    cp style.css /srv/www/css/style.css
+    echo "Directories setup under /srv/www"
+fi
+
 # python man2html.py
 mkdir -p "$BIN"
 cp man2html.py "$BIN/man2html"
